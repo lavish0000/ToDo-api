3800d682d46c0bf7dc258e300b8e4d44f719a7b5
Lavish <lovish.emilence@gmail.com>
2019-01-29 18:44:21 +0530

    added todolist

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+    ID: 1,
+    Description: 'go to market',
+    Completed: false
+},
+{
+    ID: 2,
+    Description: 'wash cloths',
+    Completed: false
+},
+{
+    ID: 3,
+    Description: 'eat food',
+    Completed: true
+}];
 
 app.get('/', function(req, res) {
     res.send('todo api root');
+});
+app.get('/todos', function(req, res) {
+    res.json(todos);
 });
 
 app.listen(PORT, function () {
