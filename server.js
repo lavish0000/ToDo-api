8cedc0b7c5882e2008b30cf8c790fbb07ba4715d
Lavish <lovish.emilence@gmail.com>
2019-01-30 16:41:14 +0530

    add todo using post

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-    ID: 1,
-    Description: 'go to market',
-    Completed: false
-},
-{
-    ID: 2,
-    Description: 'wash cloths',
-    Completed: false
-},
-{
-    ID: 3,
-    Description: 'eat food',
-    Completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+// var todos = [{
+//     ID: 1,
+//     Description: 'go to market ',
+//     Completed: false
+// },
+// {
+//     ID: 2,
+//     Description: 'wash cloths',
+//     Completed: false
+// },
+// {
+//     ID: 3,
+//     Description: 'eat food and have drinks',
+//     Completed: true
+// }];
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
     res.send('todo api root');
@@ -28,7 +33,7 @@ app.get('/todos/:id', function(req, res) {
     var todoid = parseInt(req.params.id, 10);
     var obj;
     for(i = 0; i < todos.length; i++) {
-        if(todos[i].ID === todoid) {
+        if(todos[i].id === todoid) {
              obj = todos[i];
         }
     }
@@ -36,6 +41,18 @@ app.get('/todos/:id', function(req, res) {
         res.status(404).send();
     } else
         res.json(obj);
+});
+
+app.post('/todos', function (req, res) {
+    var body = req.body;
+      
+    console.log('description ' + body.description);
+
+    body.id = todoNextId;
+    todoNextId ++;
+    todos.push(body);
+    res.json(body);
+
 });
 
 app.listen(PORT, function () {
