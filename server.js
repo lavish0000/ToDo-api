0a49b53c76bbcbb5f68b53bada6d0602bc08f84a
Lavish <lovish.emilence@gmail.com>
2019-01-31 16:52:40 +0530

    refactored api with pick underscore

--- server.js
+++ server.js
@@ -46,13 +46,14 @@ app.get('/todos/:id', function(req, res) {
 });
 
 app.post('/todos', function (req, res) {
-    var body = req.body;
+    var body = _.pick(req.body, 'description', 'completed');
       
     console.log('description ' + body.description);
 
     if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
         return res.status(400).send();
-    }
+    } 
+    body.description = body.description.trim();
 
     body.id = todoNextId ++;
     todos.push(body);
@@ -61,5 +62,5 @@ app.post('/todos', function (req, res) {
 });
 
 app.listen(PORT, function () {
-    console.log('express port' + PORT);
+    console.log('express port ' + PORT);
 });
